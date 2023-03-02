5cb4f28fbc8b5c0f8e6312fb6c869adca6b0669c
suleyman <gameclub2015@mail.ru>
2023-03-03 00:46:50 +0500

    2 lesson

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const workoutRoutes = require("./routes/workouts");
 
 const app = express();
 
 // middleware
+// ! nod olarak yazmistim
+// app.use(express.json());  req.body icin lazim veri almak icin sonraki ders icin hazirlik amacli
+
 app.use((req, res, next) => {
   console.log(req.path, req.method, req.hostname);
   next();
 });
 
-app.get("/", (req, res) => {
-  res.json({ mssg: "hello world i here" });
-});
+// routes
+app.use("/api/workouts", workoutRoutes);
 
 app.listen(process.env.PORT, (req, res) => {
   console.log("u re server started");
